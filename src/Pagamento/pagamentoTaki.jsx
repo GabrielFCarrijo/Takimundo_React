89a8ad739977f92f9e2c4f0475a1f9819512e0c9
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-11-28 15:47:17 -0300

    feat: criacao da navbar e implementação

--- src/Pagamento/pagamentoTaki.jsx
+++ src/Pagamento/pagamentoTaki.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./pagamento.css";
-
+import Navbar from "./Navbar/Navbar";
 
 function PagamentoTaki() {
     return (
+
         <div className="titulo">
+
+            <Navbar />
+
             <h1>Obrigado Por Ajudar</h1>
             <h3>Segue abaixo codigo para que seja realizada a doação</h3>
             
